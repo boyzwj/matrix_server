e4efe99903dabdbe7ab4bb3ceb337b0fda3f5f54
Tom@ZhangWenjie <boyzwj@qq.com>
2022-06-14 20:42:12 +0800

    集成manifold

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Server.MixProject do
       {:castore, ">= 0.0.0"},
       {:poison, "~> 5.0"},
       {:lz4, "~> 0.2.4", hex: :lz4_erl},
-      {:fastglobal, "~> 1.0"}
+      {:fastglobal, "~> 1.0"},
+      {:manifold, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
